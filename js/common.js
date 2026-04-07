6cbe964e79fcbc037c89097558ec2045e7646cbe
혜진 <gkagus98@naver.com>
2026-04-08 00:36:14 +0900

    0408 scroll

--- js/common.js
+++ js/common.js
@@ -1,12 +1,15 @@
-window.addEventListener("load", () => {
+document.addEventListener("DOMContentLoaded", () => {
   if (window.location.hash) {
     const el = document.querySelector(window.location.hash);
     const header = document.querySelector("header");
 
     if (el && header) {
-      const headerHeight = header.offsetHeight;
-      const y = el.getBoundingClientRect().top + window.pageYOffset - headerHeight;
-      window.scrollTo({ top: y, behavior: "smooth" });
+      setTimeout(() => {
+        const headerHeight = header.offsetHeight;
+        const y = el.getBoundingClientRect().top + window.pageYOffset - headerHeight - 10;
+
+        window.scrollTo(0, y);
+      }, 300);
     }
   }
 });
